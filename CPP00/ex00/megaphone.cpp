8fde66949c7683780ed0864882e9a497fc850bf3
JIN-LEE <jin-lee@JINHYEOKs-MacBook-Air.local>
2022-02-15 10:33:04 +0900

    feat: ex00 megaphone

---
+++ CPP00/ex00/megaphone.cpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jin-lee <jin-lee@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/15 06:00:27 by jin-lee           #+#    #+#             */
+/*   Updated: 2022/02/15 10:31:57 by jin-lee          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cctype>
+
+int	main(int argc, char **argv)
+{
+	char	*str;
+	int		idx;
+
+	idx = 0;
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else
+		while (++idx < argc)
+		{
+			str = argv[idx];
+			while (*str)
+			{
+				if (isalpha(*str) && islower(*str))
+					std::cout << (char)std::toupper(*str);
+				else
+					std::cout << *str;
+				str++;
+			}
+		}
+	std::cout << std::endl;
+	return (EXIT_SUCCESS);
+}
