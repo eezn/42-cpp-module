07d5034fcc4e2669459f4da5203882fbdd8df74d
JIN-LEE <jin-lee@JINHYEOKs-MacBook-Air.local>
2022-02-19 18:32:20 +0900

    feat: ex05 Karen

---
+++ CPP01/ex05/Karen.cpp
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Karen.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jin-lee <jin-lee@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/19 17:42:53 by jin-lee           #+#    #+#             */
+/*   Updated: 2022/02/19 18:30:42 by jin-lee          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Karen.hpp"
+
+Karen::Karen( void ) {}
+
+Karen::~Karen( void ) {}
+
+void Karen::debug( void ) {
+	std::cout << RED
+	<< "I love to get extra bacon "
+	<< "for my 7XL-double-cheese-triple-pickle-special-ketchup burger. "
+	<< "I just love it!" 
+	<< EOC << std::endl;
+}
+
+void Karen::info( void ) {
+	std::cout << GREEN
+	<< "I cannot believe adding extra bacon cost more money. "
+	<< "You don’t put enough! "
+	<< "If you did I would not have to ask for it!"
+	<< EOC << std::endl;
+}
+
+void Karen::warning( void ) {
+	std::cout << YELLOW
+	<< "I think I deserve to have some extra bacon for free. "
+	<< "I’ve been coming here "
+	<< "for years and you just started working here last month."
+	<< EOC << std::endl;
+}
+
+void Karen::error( void ) {
+	std::cout << BLUE
+	<< "This is unacceptable, "
+	<< "I want to speak to the manager now."
+	<< EOC << std::endl;
+}
+
+void Karen::complain( std::string level ) {
+
+	void (Karen::*funcPtrArr[4])( void ) = {
+		&Karen::debug,
+		&Karen::info,
+		&Karen::warning,
+		&Karen::error
+	};
+
+	std::string levelArr[4] = {
+		"DEBUG", "INFO", "WARNING", "ERROR"
+	};
+
+	for (int i = 0; i < 4; i++) {
+		if (level == levelArr[i])
+			(this->*(funcPtrArr[i]))();
+	}
+}
