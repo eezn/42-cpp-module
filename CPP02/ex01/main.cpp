890f467dfd429027f248294475b2d84b8df07575
eezn <pplaneoo@gmail.com>
2022-02-21 23:37:09 +0900

    feat: ex01

--- CPP02/ex01/main.cpp
+++ CPP02/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jin-lee <jin-lee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/20 14:07:13 by jin-lee           #+#    #+#             */
-/*   Updated: 2022/02/21 15:04:30 by jin-lee          ###   ########.fr       */
+/*   Updated: 2022/02/21 19:33:50 by jin-lee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,21 @@
 int main( void ) {
 
 	Fixed a;
-	Fixed b( a );
-	Fixed c;
+	Fixed const b( 10 );
+	Fixed const c( 42.42f );
+	Fixed const d( b );
 
-	c = b;
+	a = Fixed( 1234.4321f );
 
-	std::cout << a.getRawBits() << std::endl;
-	std::cout << b.getRawBits() << std::endl;
-	std::cout << c.getRawBits() << std::endl;
+	std::cout << "a is " << a << std::endl;
+	std::cout << "b is " << b << std::endl;
+	std::cout << "c is " << c << std::endl;
+	std::cout << "d is " << d << std::endl;
+
+	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
 	
 	return (0);
 }
