d73b43fa7490e340e022121a733ca8e1d5b6db21
eezn <pplaneoo@gmail.com>
2022-03-04 01:25:15 +0900

    style(CPP02)

--- CPP02/ex00/Fixed.cpp
+++ CPP02/ex00/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: jin-lee <jin-lee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/20 14:06:57 by jin-lee           #+#    #+#             */
-/*   Updated: 2022/02/21 12:42:50 by jin-lee          ###   ########.fr       */
+/*   Updated: 2022/03/04 01:24:09 by jin-lee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,13 +31,13 @@ Fixed::~Fixed( void ) {
 
 int Fixed::getRawBits( void ) const {
 
-	std::cout << "getRawBits called" << std::endl;
+	std::cout << "getRawBits member function called" << std::endl;
 	return (this->value);
 }
 
 void Fixed::setRawBits( int const raw ) {
 
-	std::cout << "setRawBits called" << std::endl;
+	std::cout << "setRawBits member function called" << std::endl;
 	this->value = raw;
 }
 
