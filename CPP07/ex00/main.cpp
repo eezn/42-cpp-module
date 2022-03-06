fcafd45e6b10d0c4dfab719a0b8f893d7b4aa7e9
eezn <pplaneoo@gmail.com>
2022-03-07 01:39:27 +0900

    feat(CPP07_ex00): Template

---
+++ CPP07/ex00/main.cpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jin-lee <jin-lee@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/07 01:25:26 by jin-lee           #+#    #+#             */
+/*   Updated: 2022/03/07 01:38:05 by jin-lee          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "whatever.hpp"
+
+int main ( void ) {
+
+	int a = 2;
+	int b = 3;
+
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min(a, b) = " << ::min(a, b) << std::endl;
+	std::cout << "max(a, b) = " << ::max(a, b) << std::endl;
+
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+
+	::swap( c, d );
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min(c, d) = " << ::min(c, d) << std::endl;
+	std::cout << "max(c, d) = " << ::max(c, d) << std::endl;
+
+	float e = 42.0f;
+	float f = 42.42f;
+
+	::swap( e, f );
+	std::cout << "e = " << e << ", f = " << f << std::endl;
+	std::cout << "min(e, f) = " << ::min(e, f) << std::endl;
+	std::cout << "max(e, f) = " << ::max(e, f) << std::endl;
+
+	return (0);
+
+}
